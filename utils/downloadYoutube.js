16101c576f460a454efdc7e5dd9837beaf49f337
EitanSteven <eitansteven2002@hotmail.com>
2025-03-21 11:09:59 -0300

    yt-dlp mozilla user-agent

--- utils/downloadYoutube.js
+++ utils/downloadYoutube.js
@@ -1,7 +1,5 @@
 import { exec } from 'child_process'
 import util from 'util'
-import { unlink } from 'fs/promises'
-import path from 'path'
 
 const execPromise = util.promisify(exec)
 
@@ -18,7 +16,7 @@ export const downloadVideo = async (videoId, url, outputFilePath, mediaFormat) =
 
     if (mediaFormat === 'mp4') {
       // Descarga y fusión automática con yt-dlp
-      const videoCommand = `yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]" --merge-output-format mp4 --output "${outputFilePath}" ${url}`
+      const videoCommand = `yt-dlp --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" --referer "https://www.youtube.com/" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]" --merge-output-format mp4 --output "${outputFilePath}" ${url}`
       console.log('Ejecutando comando de video:', videoCommand)
       await execPromise(videoCommand)
       return true
